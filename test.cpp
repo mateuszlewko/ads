804f80f436f7b2e3de2f1955fa4bd69a9a0bf70b
Mateusz <mateusz.lewko@gmail.com>
2020-01-29 01:15:30 +0100

    All coding done

--- test.cpp
+++ test.cpp
@@ -67,91 +67,20 @@ int main() {
   std::shared_ptr<fast_set<int>> sw = std::make_shared<set_wrapper<int>>(u_exp);
 
   srand(123);
-  // for (int i = 0; i < 100000; i++) {
-  //   int x = rand() % 100000;
-  //   int y = rand() % 100000;
-
-  //   std::cout << x << ", ";
-
-  //   sw->insert(x);
-  //   yf->insert(x);
-  //   int a1 = sw->succ(y);
-  //   std::cout << "-----\n";
-  //   int a2 = yf->succ(y);
-
-  //   if (a1 != a2) {
-  //     std::cout << "WA on query = " << y << "\n";
-  //     std::cout << "\n" << a1 << "\n" << a2 << std::endl;
-  //     return 1;
-  //   }
-  // }
-
-  // for (int y : data) {
-  //   printf("%d\n%d\n--------\n", yf->succ(y), sw->succ(y));
-  // }
 
   std::cout << "Initalized data structures.";
 
-  int N[] = {10000, 1000000, 10000000, 15000000};
+  int N[] = {10000, 1000000, 8000000, 12000000};
 
   for (int i = 0; i < 4; i++) {
     int n = N[i];
 
     init_test1(n, u);
-    std::cout << "output xor = " << test1(veb, n, "vEB") << std::endl;
-    std::cout << "output xor = " << test1(xf, n, "x-fast") << std::endl;
-    std::cout << "output xor = " << test1(yf, n, "y-fast") << std::endl;
-    std::cout << "output xor = " << test1(sw, n, "std::set") << std::endl;
+
+    std::cout << "output xor = " + test1(veb, n, "vEB") << std::endl;
+    std::cout << "output xor = " + test1(xf, n, "x-fast") << std::endl;
+    std::cout << "output xor = " + test1(yf, n, "y-fast") << std::endl;
+    std::cout << "output xor = " + test1(sw, n, "std::set") << std::endl;
   }
 
-  // init_test1(100000, u);
-  // cout << test1(veb, 100000)
-
-  //         init_test1(1000000, u);
-
-  // init_test1(10000000, u);
-
-  // init_
-
-  // srand(123);
-
-  // std::vector<int> vs;
-
-  // for (int i = 0; i < 10000; i++) {
-  //   int x = rand() % (u / 2);
-  //   veb.insert(x);
-  //   xf.insert(x);
-  //   yf.insert(x);
-  //   sw.insert(x);
-
-  //   vs.push_back(x);
-  // }
-
-  // for (int i = 0; i < 100; i++) {
-  //   int x = rand() % (u / 2);
-
-  //   int a1 = veb.succ(x);
-  //   int a2 = xf.succ(x);
-  //   int a3 = yf.succ(x);
-  //   int a4 = sw.succ(x);
-
-  //   std::cout << a1 << "\n" << a2 << "\n" << a3 << "\n" << a4 <<
-  //   "\n--------\n";
-  // }
-
-  // s.insert(4);
-  // s.insert(10);
-  // s.insert(11);
-  // s.insert(1000);
-  // s.insert(500001);
-
-  // std::cout << "succ(1) = " << s.succ(1) << std::endl
-  //           << "succ(9) = " << s.succ(9) << std::endl
-  //           << "succ(10) = " << s.succ(10) << std::endl
-  //           << "succ(11) = " << s.succ(11) << std::endl
-  //           << "succ(510000) = " << s.succ(510000) << std::endl
-  //           << "succ(1000) = " << s.succ(1000) << std::endl
-  //           << "find(1000) = " << s.lookup(1000) << std::endl
-  //           << "find(500001) = " << s.lookup(500001) << std::endl
-  //           << "find(1) = " << s.lookup(1) << std::endl;
 }
